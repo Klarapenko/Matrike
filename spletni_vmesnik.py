7661c168c5078f48d3a7dd2d4126bbbcbe03fbda
Klara Penko <klara.penko@gmail.com>
2020-08-18 20:16:43 +0200

    definicijo za razumevanje matrike prestavila v model.py

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -1,78 +1,103 @@
-from model import Matrika
+from model import Matrika, razumevanje_matrike
+import model
 import bottle
 
-#def sprejem_matrike(mat):
-    ### še nevem kako se bo vpisovalo v streznik##
-    #dopolnim kasnej
 
-
-@bootle.get('/')
+@bottle.get('/')
 def osnovna_stran():
-    bottle.template('osnovna_stran.tpl')
-
-
-@bottle.get('/izbirna_stran/')
-    def naslednja_stran():
-        bottle.template('naslednja_stran.tpl')
-
+    return bottle.template('osnovna_stran.tpl')
 
 @bottle.get('/sestej/')
 def sestej():
-    pass
+    return bottle.template('seštevanje.tpl')
+    
 
 @bottle.post('/sestevanje/')
-
-
+def sestevaj():
+    mat1 = bottle.request.forms['matrika1']
+    mat2 =  bottle.request.forms['matrika2']
+    rezultat = razumevanje_matrike(mat1) + razumevanje_matrike(mat2)
+    return bottle.template("rezultat.tpl", rezultat = rezultat)
 
 
 
 @bottle.get('/odstej/')
 def odstej():
-    pass
+    return bottle.template('odštevanje.tpl')
 
 @bottle.post('/odstevanje/')
-
-
-
-@bottle.get('/transponiraj/')
-def transponiraj():
-    pass
-
-@bottle.post('/transponiranje/')
-
-
+def odstevaj():
+    mat1 = bottle.request.forms['matrika1']
+    mat2 =  bottle.request.forms['matrika2']
+    rezultat = razumevanje_matrike(mat1) - razumevanje_matrike(mat2)
+    return bottle.template("rezultat.tpl", rezultat = rezultat)
 
 @bottle.get('/zmnozi_matriki/')
 def zmnozi_matriki():
-    pass
+    return bottle.template("mnozenje_matrik.tpl")
 
 @bottle.post('/mnozenje_matrik/')
-
-
-
-
-@bottle.get('/kvadriraj/')
-def kvadriraj():
-    pass
-
-@bottle.post('/kvadriranje/')
-
-
-
-@bottle.get('/izracunaj_sled/')
-def sled():
-    pass
-
-@bottle.post('/racunanje_sledi/')
+def mnozi():
+    mat1 = bottle.request.forms['matrika1']
+    mat2 =  bottle.request.forms['matrika2']
+    rezultat = razumevanje_matrike(mat1) * razumevanje_matrike(mat2)
+    return bottle.template("rezultat.tpl",rezultat = rezultat)
 
 
 
 @bottle.get('/zmnozi_skalarjem/')
 def zmnozi_s_skalarjem():
-    pass
+    return bottle.template("mnozenje_skalar.tpl")
 
 @bottle.post('/mnozenje_s_skalarjem/')
+def zmnozi():
+    mat1 = bottle.request.forms['matrika1']
+    skalar = bottle.request.forms['skalar']
+    rezultat = razumevanje_matrike(mat1).zmnozek_matrika_s_skalarjem(skalar)
+    return bottle.template("rezultat.tpl", rezultat = rezultat)
+
+
+
+
+
+#@bottle.get('/kvadriraj/')
+#def kvadrat():
+#    return bottle.template("kvadriranje_matrike.tpl")
+#
+#@bottle.post('/kvadriranje/')
+#def kvadriraj():
+#    mat1 = bottle.request.forms['matrika1']
+#    rezultat = razumevanje_matrike(mat1).kvadrat_matrike()
+#    return bottle.template("rezultat.tpl", rezultat = rezultat)
+
+
+
+
+@bottle.get('/transponiraj/')
+def transponiranka():
+    return bottle.template("transponiranje.tpl")
+
+@bottle.post('/transponiranje/')
+def transponiraj():
+    mat1 = bottle.request.forms['matrika1']
+    rezultat = razumevanje_matrike(mat1).transponiraj()
+    return bottle.template("rezultat.tpl", rezultat = rezultat)
+
+
+@bottle.get('/izracunaj_sled/')
+def sled():
+    return bottle.template("izracunaj_sled.tpl")
+
+@bottle.post('/racunanje_sledi/')
+def racunaj_sled():
+    mat1 = bottle.request.forms['matrika1']
+    rezultat = razumevanje_matrike(mat1).sled_matrike()
+    return bottle.template("rezultat_sled.tpl", rezultat = rezultat)
+
+
    
+
+bottle.run(debug= True, reloader= True)
 
 
 
